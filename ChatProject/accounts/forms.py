fb5374688845e09d81e682e655c8efece29f99ce
a-im12 <ayumu.git@gmail.com>
2023-05-28 14:58:31 +0900

    CREATE:chatapp

--- ChatProject/accounts/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from . import models
-
-class CreateUserForm(ModelForm):
-    class Meta:
-        model = models.CustomUserModel
-        fields = ['username', 'email', 'password']
--- ChatProject/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'chat'
-
-urlpatterns = [
-    path('mypage/', views.MyPageView.as_view(), name='mypage'),
-]
